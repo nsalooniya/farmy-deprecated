8ad1ceb44bac94429ccd989e5a649822b39ba501
Nikhil Salooniya <saloniyanikhil@gmail.com>
2021-05-06 18:16:15 +0530

    view added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Router = require('./lib/router');
 const $ = require('./lib/adm');
+const View = require('./lib/view');
 const HP = require('./lib/helper');
 
 module.exports = {
     Router,
     $,
+    View,
     HP
 };
