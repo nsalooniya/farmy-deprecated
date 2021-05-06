523fd838ad10673d96c5894b01cc1b81e219f61f
Nikhil Salooniya <saloniyanikhil@gmail.com>
2021-05-06 20:36:30 +0530

    state added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Router = require('./lib/router');
 const $ = require('./lib/adm');
 const View = require('./lib/view');
+const State = require('./lib/state');
 const HP = require('./lib/helper');
 
 module.exports = {
     Router,
     $,
     View,
+    State,
     HP
 };
