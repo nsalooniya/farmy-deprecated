12c7cc9c58a271742d59df4bbe79801bbdad8e80
Nikhil Salooniya <saloniyanikhil@gmail.com>
2021-05-16 21:33:28 +0530

    added component test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Router = require('./lib/router');
 const $ = require('./lib/adm');
 const View = require('./lib/view');
+const Component = require('./lib/component');
 const State = require('./lib/state');
 const HP = require('./lib/helper');
 
@@ -8,6 +9,7 @@ module.exports = {
     Router,
     $,
     View,
+    Component,
     State,
     HP
 };
