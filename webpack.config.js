7b81f29a96cd97f3c63ed2a193aa463149c9b7c8
salooniya <saloniyanikhil@gmail.com>
2021-12-15 14:44:00 +0530

    setup working

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require('path');
+import path from 'path';
+const __dirname = path.resolve();
 
-module.exports = {
+export default {
     mode: 'development',
     entry: {
         app: './test/src/app.js'
